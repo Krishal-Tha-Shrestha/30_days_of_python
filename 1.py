6f92fa2c88519bda2c092136a678e4fcf78262d2
Krishal-Tha-Shrestha <krishaltha.stha@gmail.com>
2026-06-06 08:07:25 -0700

    learing python in 30 days

---
+++ 1.py
@@ -0,0 +1,9 @@
+#Day 1: 30 days of python programming
+#to find eclidian distance between (2,3) and (10,8)
+
+x1=2
+y1=3
+x2=10
+y2=8
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print("The eclidian distance is:",float('%.2f' % distance))
---
+++ 2.py
@@ -0,0 +1,28 @@
+#Day 2: 30 days of python programming
+first_name="Asabeneh"
+last_name="Yetayeh" 
+full_name=first_name+" "+last_name
+country="Finland"   
+city="Helsinki"
+age=2
+year=2083
+is_married=False
+is_true=True
+is_light_on=False
+details={"first_name":"Asabeneh","last_name":"Yetayeh","country":"Finland","city":"Helsinki"}
+print("details:",details)
+#multiple variable assignment
+name,age,country="Asabeneh",250,"Finland"
+print(name,age,country)
+print("length of first name:",len(first_name))
+print("data type of variables:")
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
